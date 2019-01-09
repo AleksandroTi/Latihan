f65a86c38c5cedb82233302d99103591c15dd51a
AleksandroTi <44310089+AleksandroTi@users.noreply.github.com>
2019-01-09 19:44:58 +0700

    Update contact.php

--- asset/contact.php
+++ asset/contact.php
@@ -1,3 +1,37 @@
+<html>
+<body>
+<style>
+body {
+  background-color: yellow;
+}
+
+h1 {
+  color: yellow;
+  text-align: left;
+  font-size: 22px;
+}
+
+h2 {
+  color: red;
+  text-align: left;
+  font-family: wide latin;
+  font-size: 25px;
+}
+</style>
 <?php
- // You have to download the Premium version to get working Contact form alongwith full documentation of the  template
+ // You have to download the Premium version to get working contact form alongwith full documentation of the  template
+$Nama	=$_POST['Nama']; 
+$email	=$_POST['email'];
+$Subjek =$_POST['Subjek'];
+$Pesan 	=$_POST['Pesan']
 ?>
+<h2>
+<?php
+echo "Nama Depan	= $Nama<br>";
+echo "Email			= $email<br>";
+echo "Subjek		= $Subjek<br>";
+echo "Pesan			= $Pesan<br>";
+?>
+</h2>
+</body>
+</html>
